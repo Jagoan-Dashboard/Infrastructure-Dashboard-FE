0b0135cebb902aa1f708e497798be9ddbf7f66ad
prakosoee <elangprakoso088@gmail.com>
2025-10-16 15:33:02 +0700

    fix: fixing image from s3

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-backend-dashboard.jagoansatudata.com",
+        port: "",
+        pathname: "/jagoanbe/reports/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
